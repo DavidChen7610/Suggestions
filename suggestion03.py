6e03d0a56423af696b9e121b9fe7baddb5b33b52
florije1988 <florije1988@gmail.com>
2014-06-28 21:27:43 +0800

    这是编写高质量代码之改善python程序的91个建议的配套学习笔记，

---
+++ suggestion03.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+__author__ = 'yanghua'
+
+'''
+理解python与C语言的不同之处
+
+1/缩进和{}
+2/'与""
+3/三元操作符"?:"
+4/switch...case
+
+'''
+
+
+def f(num):
+    return {
+        0: "The first one!",
+        1: "The second one!",
+        2: "The third one!"
+    }.get(num, "No vainly!")
+
+if __name__ == '__main__':
+    print f(2)
