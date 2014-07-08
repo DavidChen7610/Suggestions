28724761603144d03485acddc8df96f665926236
florije1988 <florije1988@gmail.com>
2014-07-08 14:55:10 +0800

    fabric 简单的入门

---
+++ others/fabfile.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__author__ = 'florije'
+from fabric.api import *
+
+# env.hosts = ['106.186.117.251:22']
+# env.user = "florije"
+# env.password = '903326'
+
+
+def hello():
+    print("Hello fabric!")
+
+'''
+D:\Projects\python\suggestion_env\Scripts\fab-script.py
+-f fabfile hello
+D:\Projects\python\Suggestions\others
+'''
