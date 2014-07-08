bc34252ae0e6254c0c7c1f04cfd921aebe7078fc
florije1988 <florije1988@gmail.com>
2014-07-08 15:34:06 +0800

    fabric 简单的入门

--- others/fabfile.py
+++ others/fabfile.py
@@ -10,8 +10,16 @@ env.password = '***'
 def hello():
     print("Hello fabric!")
 
+
 '''
 D:\Projects\python\suggestion_env\Scripts\fab-script.py
 -f fabfile hello
 D:\Projects\python\Suggestions\others
 '''
+
+
+def remote():
+    '''远程文件拷贝操作'''
+    run('pwd')
+    run('cp /home/florije/tmp/text.txt /home/florije/tmp/xiaoqi.txt')
+    print 'remote copy file success'
