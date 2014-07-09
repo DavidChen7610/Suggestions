2e99d50bf8e967b47adbbd7a7a3161542065583a
florije1988 <florije1988@gmail.com>
2014-07-09 17:49:55 +0800

    第一次进行操作

--- cookbook/cookbook004.py
+++ cookbook/cookbook004.py
@@ -4,3 +4,9 @@ __author__ = 'florije'
 '''
 Finding the Largest or Smallest N Items
 '''
+
+import heapq
+
+nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+print(heapq.nlargest(3, nums))  # Prints [42, 37, 23]
+print(heapq.nsmallest(3, nums)) # Prints [-4, 1, 2]
