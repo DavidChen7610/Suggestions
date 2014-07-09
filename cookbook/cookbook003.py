fb3982f9c84f763ec57cdd06975d40bd06f73e4c
florije1988 <florije1988@gmail.com>
2014-07-09 17:44:53 +0800

    cookbook004.py, Finding the Largest or Smallest N Items

--- cookbook/cookbook003.py
+++ cookbook/cookbook003.py
@@ -61,6 +61,6 @@ if __name__ == '__main__':
     with open('somefile.txt') as f:
         for line, prevlines in search(f, 'python', 5):
             for pline in prevlines:
-                print pline, ','
-            print line, ','  # print(line, end='')
+                print pline
+            print line  # print(line, end='')
             print('-'*20)
---
+++ cookbook/cookbook004.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__author__ = 'florije'
+
+'''
+Finding the Largest or Smallest N Items
+'''
