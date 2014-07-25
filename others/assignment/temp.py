e769185e708ff6cff07a6c58f4ababfde01518e9
florije1988 <florije1988@gmail.com>
2014-07-25 18:17:33 +0800

    添加suggestion32.py,警惕默认参数潜在的问题

---
+++ others/assignment/temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'florije'
+
+if __name__ == '__main__':
+    # s_arg = input('list:')
+    # print s_arg
+    # print type(s_arg)
+
+    for i in range(1, 20):
+        print '%02d' % i
+
+    print "Age:%02d" % 1
+    title = ''
+    f =file("%s.html" % title, "a")
